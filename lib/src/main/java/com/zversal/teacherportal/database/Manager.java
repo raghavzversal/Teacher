c16b8df64e47ff2f52943a548ce8b04f9c002baa
Raghav Sharma <raghav.zversal@gmail.com>
2021-10-04 12:53:44 +0530

    updation of port

--- lib/src/main/java/com/zversal/teacherportal/database/Manager.java
+++ lib/src/main/java/com/zversal/teacherportal/database/Manager.java
@@ -9,8 +9,7 @@ import java.util.Properties;
 
 public class Manager
 {
-
-    public static Properties loadPropertiesFile() throws Exception {
+	public static Properties loadPropertiesFile() throws Exception {
  
         Properties prop = new Properties();
         InputStream in = new FileInputStream("database.properties");
@@ -20,21 +19,19 @@ public class Manager
     }
 
     public Connection con = null;
+    public int port = 0;
     public Manager()
     {
             try {
 
                 Properties prop = loadPropertiesFile();
-
                 String driverClass = prop.getProperty("test.jdbc.dev.driver");
                 String url = prop.getProperty("test.jdbc.dev.url");
                 String username = prop.getProperty("test.jdbc.dev.username");
                 String password = prop.getProperty("test.jdbc.dev.password");
-
                 Class.forName(driverClass);
-
                 con = DriverManager.getConnection(url, username, password);
-
+                port = Integer.parseInt(prop.getProperty("PORT"));
                 }catch (SQLException e) {
                 e.printStackTrace();
                 } 
--- lib/src/main/java/com/zversal/teacherportal/main/Main.java
+++ lib/src/main/java/com/zversal/teacherportal/main/Main.java
@@ -4,19 +4,23 @@ import static spark.Spark.after;
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.put;
+import java.io.IOException;
 import static spark.Spark.delete;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.zversal.teacherportal.controller.Controller;
+import com.zversal.teacherportal.database.Manager;
 
+import static spark.Spark.port;
 
 public class Main {
-	public static void main(String[] args) 
+	public static Manager getPort = new Manager();
+	public static void main(String[] args) throws IOException 
 	{
-		
 		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 		path("/user", () -> 
 		{
+			port(getPort.port);
 			post("/add", Controller.add, gson::toJson );
 			get("/find/:id", Controller.find, gson::toJson);
 			put("/update", Controller.update, gson::toJson);
